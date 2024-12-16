d355746911fb57cafaf028da0d9e21793a951f66
Goodbird <goodbirdmail@yandex.ru>
2024-12-16 18:23:20 +0300

    Minor fixes

--- src/main/java/mod/beethoven92/betterendforge/common/block/TenaneaFlowersBlock.java
+++ src/main/java/mod/beethoven92/betterendforge/common/block/TenaneaFlowersBlock.java
@@ -6,15 +6,19 @@ import mod.beethoven92.betterendforge.common.block.template.EndVineBlock;
 import mod.beethoven92.betterendforge.common.util.AdvMathHelper;
 import mod.beethoven92.betterendforge.common.util.ModMathHelper;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockGrass;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class TenaneaFlowersBlock extends EndVineBlock {
 	public static final Vec3i[] COLORS;
@@ -71,5 +75,11 @@ public class TenaneaFlowersBlock extends EndVineBlock {
 	@Override
 	public boolean isPassable(IBlockAccess p_176205_1_, BlockPos p_176205_2_) {
 		return true;
+	}
+
+	@Nullable
+	public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos)
+	{
+		return NULL_AABB;
 	}
 }
