653771bc8bef6b30371773e360a1221926383ae0
Goodbird <goodbirdmail@mail.com>
2025-08-24 17:23:59 +0300

    Added pallidium cover bonemealing

--- src/main/java/mod/beethoven92/betterendforge/common/block/PallidiumBlock.java
+++ src/main/java/mod/beethoven92/betterendforge/common/block/PallidiumBlock.java
@@ -3,11 +3,17 @@ package mod.beethoven92.betterendforge.common.block;
 import mod.beethoven92.betterendforge.common.init.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockBush;
+import net.minecraft.block.IGrowable;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
-public class PallidiumBlock extends Block
+import java.util.Random;
+
+public class PallidiumBlock extends Block implements IGrowable
 {
 	private final Block nextLevel;
 
@@ -23,5 +29,20 @@ public class PallidiumBlock extends Block
 	public boolean canBePotted()
 	{
 		return this == ModBlocks.PALLIDIUM_FULL;
+	}
+
+	@Override
+	public boolean canGrow(World world, BlockPos blockPos, IBlockState iBlockState, boolean b) {
+		return nextLevel!=null;
+	}
+
+	@Override
+	public boolean canUseBonemeal(World world, Random random, BlockPos blockPos, IBlockState iBlockState) {
+		return nextLevel!=null;
+	}
+
+	@Override
+	public void grow(World world, Random random, BlockPos blockPos, IBlockState iBlockState) {
+		world.setBlockState(blockPos, nextLevel.getDefaultState(), 3);
 	}
 }
